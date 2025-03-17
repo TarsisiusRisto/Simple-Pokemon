b6d7f5d803da4bc3d497a4291eb9e7abdd37b47f
TarsisiusRisto <risto.tarsisius@gmail.com>
2025-03-17 17:06:41 +0700

    add player

--- Player.java
+++ Player.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public class Player {
-    
+    ArrayList<Pokemon> pokeDex = new ArrayList<>();
+    int px, py;
+    String name;
+    int pokeBalls = 10;
+
+    Player(String name){
+        this.px = 0;
+        this.py = 0;
+        this.name = name;
+    }
 }
